afa82d4c9029c80a60320c7d217355b1a5b1594d
Corey Barr <corey.jbarr@gmail.com>
2020-03-03 13:30:54 -0600

    better sql

--- joins-solution.sql
+++ joins-solution.sql
@@ -3,18 +3,36 @@
 SELECT * FROM customers JOIN addresses ON customers.id = addresses.customer_id;
 
 -- Get all orders and their line items (orders, quantity and product).
-SELECT * FROM orders JOIN line_items ON orders.id = line_items.order_id;
-
+SELECT * FROM orders JOIN line_items ON orders.id = line_items.order_id JOIN products ON line_items.product_id = products.id;
 -- Which warehouses have cheetos?
 SELECT warehouse_id FROM warehouse_product WHERE product_id = 5;
 
 -- Which warehouses have diet pepsi?
+
+SELECT warehouse_id FROM warehouse_product WHERE product_id = 6;
+
 -- Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+SELECT first_name, last_name, count(orders.id)
+FROM customers
+JOIN addresses
+ON customers.id = addresses.customer_id 
+JOIN orders
+ON addresses.id = orders.address_id
+GROUP BY customers.first_name, customers.last_name
+;
+
 -- How many customers do we have?
+SELECT COUNT(*) FROM customers;
+
 -- How many products do we carry?
+
+SELECT COUNT(*) FROM products;
 -- What is the total available on-hand quantity of diet pepsi?
+SELECT SUM(on_hand) FROM warehouse_product WHERE product_id = 6 ;
+
 -- Stretch
 -- How much was the total cost for each order?
+
 -- How much has each customer spent in total?
 -- How much has each customer spent in total? Customers who have spent $0 should still
 
